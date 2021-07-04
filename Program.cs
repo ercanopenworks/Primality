ed889898ef35180b6fb7c6ec00acb6d2d6a063f5
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-04 18:01:02 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Primality
+{
+    class Program
+    {
+        public static string primality(int n)
+        {
+            if (n < 2) return "Not prime";
+            else if (n == 2) return "Prime";
+            else if (n % 2 == 0) return "Not prime";
+
+            var sqrt = (int)Math.Sqrt(n);
+
+            for (var i = 3; i <= sqrt; i += 2)
+            {
+                if (n % i == 0)
+                    return "Not prime";
+
+            }
+
+            return "Prime";
+        }
+        static void Main(string[] args)
+        {
+            int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int pItr = 0; pItr < p; pItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                string result = primality(n);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
